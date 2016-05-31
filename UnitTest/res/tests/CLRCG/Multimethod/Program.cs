8c91ac4c34d97125570cc77d75f8fe5ab459e386
miguelgrdotcom <miguel@miguelgr.com>
2016-05-31 17:35:10 +0200

    Multimethod test completed

--- UnitTest/res/tests/CLRCG/Multimethod/Program.cs
+++ UnitTest/res/tests/CLRCG/Multimethod/Program.cs
@@ -25,7 +25,14 @@ namespace Multimethod
             Console.WriteLine("3+StaDyn = {0}", result);
             result = Evaluate(stringValue, new AndOperator(), doubleValue);
             Console.WriteLine("StaDyn && 23.34 = {0}", result);
-            //result = Evaluate(boolValue, doubleValue, stringValue); //Compilation error                  
+            //result = Evaluate(boolValue, doubleValue, stringValue); //Compilation error            
+            dynamic union;
+            if (args.Length > 0)
+                union = integerValue;
+            else
+                union = doubleValue;
+            result = Evaluate(union, new AddOperator(), union);
+            Console.WriteLine(union.ToString() + " + " + union.ToString() + " = " + result.ToString());
             Console.WriteLine("Successful!!");
         }
     }
