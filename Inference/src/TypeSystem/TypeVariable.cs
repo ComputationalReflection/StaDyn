bf94932e6b6283e50d624b105faffa19b84aae01
miguelgrdotcom <miguel@miguelgr.com>
2016-10-20 18:49:25 +0200

    Fixed bug. Collection was modified; enumeration operation may not execute.

--- Inference/src/TypeSystem/TypeVariable.cs
+++ Inference/src/TypeSystem/TypeVariable.cs
@@ -373,8 +373,8 @@ namespace TypeSystem {
             TypeVariable newTypeVariable = (TypeVariable)this.CloneTypeVariables(typeVariableMappings, equivalenceClasses, new List<ClassType>());
             for (int i = equivalenceClasses.Count - 1; i >= 0; i--)
                 if (equivalenceClasses[i].TypeVariables.Count == 1 && equivalenceClasses[i].TypeVariables.ContainsKey(this.Variable))
-                    equivalenceClasses.RemoveAt(i);
-            foreach (EquivalenceClass equivalenceClass in equivalenceClasses)
+                    equivalenceClasses.RemoveAt(i);          
+            foreach (EquivalenceClass equivalenceClass in new List<EquivalenceClass>(equivalenceClasses))
                 equivalenceClass.UpdateEquivalenceClass(typeVariableMappings);
             newTypeVariable.ValidTypeExpression = false;
             return newTypeVariable;
--- Wrong Test Cases/Case X/CG.cs
+++ Wrong Test Cases/Case X/CG.cs
@@ -11,26 +11,94 @@ namespace Points
             this.data = data;
             this.next = next;
         }
-        public override string ToString()
+
+        public override String ToString()
         {
-            return "Node[data=" + data.ToString() + ",next=" + next.ToString() + "]";
+            return "Node [data:" + data + ",next:" + next + "]";
+        }
+    }
+
+
+    public class Point3D
+    {
+        public var x;
+        public var y;
+        public var z;
+        public var dimensions;
+        public Point3D(var x, var y, var z, var dimensions)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.dimensions = dimensions;
+        }
+
+        public override String ToString()
+        {
+            return "3D [x:" + x + ",y:" + y + ",z:" + z + "]";
+        }
+    }
+
+    public class Point2D
+    {
+        public var x;
+        public var y;
+        public var dimensions;
+        public Point2D(var x, var y, var dimensions)
+        {
+            this.x = x;
+            this.y = y;
+            this.dimensions = dimensions;
+        }
+
+        public override String ToString()
+        {
+            return "2D [x:" + x + ",y:" + y + "]";
+        }
+    }
+
+    public class Points
+    {
+        private var createPoint(var dimensions, var x, var y, var z)
+        {
+            var point;
+            if (dimensions == 2)
+                point = new Point2D(x, y, 2);
+            else
+                point = new Point3D(x, y, z, 3);
+            return point;
+        }
+
+        private var createPoints(var number)
+        {
+            var list;
+            var point;
+            var i;
+            point = createPoint(3, 0, 0, 0);
+            list = new Node(point, 0);
+            i = 1;
+            while (i < number)
+            {
+                point = createPoint(2, 0, 0, 0);
+                list = new Node(point, list);
+                i = i + 1;
+            }
+            return list;
+        }
+
+        public void Run()
+        {
+            var list = createPoints(3);
+            System.Console.WriteLine("Result {0}", list);
         }
     }
 
     public class Program
     {
-        public static var PositiveData(var list)
-        {
-            var fuyu = list.next;
-			System.Console.WriteLine("Result {0}", fuyu);
-			return list.next;
-        }
-
         public static void Main(string[] args)
         {
-            var list = new Node(1, new Node(2, 3));
-            var result = Program.PositiveData(list);
-            System.Console.WriteLine("Result {0}", result);
+            Points points = new Points();
+            points.Run();
             Console.WriteLine("Successful!!");
         }
     }
